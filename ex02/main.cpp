896b7621414b36d7e1352ff2f1472dd7b8e61095
lucille <lucille.burnet@gmail.com>
2022-01-13 13:05:16 +0100

    ok

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -7,7 +7,10 @@ int	main(){
 	std::string	&stringREF = str;
 
 	std::cout << "\t  str's address = " << &str << std::endl;
-	std::cout << "using stringPTR address = " << &stringPTR << std::endl;
+	std::cout << "using stringPTR address = " << stringPTR << std::endl;
 	std::cout << "using stringREF address = " << &stringREF << std::endl;
+	std::cout << "\t  str's = " << str << std::endl;
+	std::cout << "using stringPTR = " << *stringPTR << std::endl;
+	std::cout << "using stringREF = " << stringREF << std::endl;
 	return (0);
 }
