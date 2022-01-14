43583f81aabfebad5590cff574768c0591bb6d83
lucille <lucille.burnet@gmail.com>
2022-01-14 17:25:46 +0100

    a check

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@ int	check_av(int ac, char **av)
 {
 	(void)av;
 	if (ac < 4 || av[2][0] == 0 || av[3][0] == 0){
-		std::cout << "Please enter a filename ans 2 strings not empty"
+		std::cout << "Please enter a filename and 2 strings not empty"
 			<< std::endl;
 		return (1);
 	}
